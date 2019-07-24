d1583dafeacea8b79cfe25baded38f8f466c6680
Leon <l1anghc@163.com>
2019-07-24 11:26:41 +0800

    第一次提交

---
+++ yuan-http/src/main/java/cn/leon/yuan/http/exec/ExecuteHandler.java
@@ -0,0 +1,13 @@
+package cn.leon.yuan.http.exec;
+
+/**
+ * @Author: lianghc
+ * @Description:
+ * @Date: 2019/7/24 11:23
+ * @Version: 1.0
+ */
+public interface ExecuteHandler {
+
+    ExecuteResult excute();
+
+}
---
+++ yuan-http/src/main/java/cn/leon/yuan/http/exec/ExecuteResult.java
@@ -0,0 +1,17 @@
+package cn.leon.yuan.http.exec;
+
+import lombok.Data;
+
+/**
+ * @Author: lianghc
+ * @Description:
+ * @Date: 2019/7/24 11:24
+ * @Version: 1.0
+ */
+
+@Data
+public class ExecuteResult {
+
+    private String name;
+
+}
